2a5ac177483b579d11612977b85a1c19118cc612
Dhubleid <42750555+Dhubleid@users.noreply.github.com>
2018-08-27 16:21:02 -0300

    Create agenda.c

---
+++ agenda.c
@@ -0,0 +1,32 @@
+#inlcude<stdio.h>
+#include<stdlib.h>
+#define MAX 100
+
+listar(int *pal){
+
+}
+
+novo(int *pal, int *n, char *nome){
+    *n++;
+    printf("\nDigite o nome:\n")
+    fgets(nome,"30",stdin);
+    pal=(realloc(n*sizeof(int)+(n*(30*sizeof(char)))));
+}
+
+int main(){
+
+int *i, *n,*pal;
+char *nome[30];
+
+pal=(malloc(1*sizeof(int));
+
+n=&pal[0];
+*n=0;
+
+
+
+printf("Digite o numeros de pessoas");
+
+
+
+
