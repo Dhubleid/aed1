fd8972bc46efb2c326ed4013203fa54e81ec1735
Dhubleid <42750555+Dhubleid@users.noreply.github.com>
2018-10-01 14:48:22 -0300

    Update agenda.c

--- agenda.c
+++ agenda.c
@@ -1,32 +1,120 @@
-#inlcude<stdio.h>
-#include<stdlib.h>
-#define MAX 100
+#include <stdio.h>
+#include <stdlib.h>
 
-listar(int *pal){
+void novoContato(void *pB);
+void buscaContato(void *pB);
+void apagarContato(void *pB);
+void listarContatos(void *pB);
+void ordenaçãoBF(void *pB);
+void ordenaçaoInsert(void *pB);
 
-}
 
-novo(int *pal, int *n, char *nome){
-    *n++;
-    printf("\nDigite o nome:\n")
-    fgets(nome,"30",stdin);
-    pal=(realloc(n*sizeof(int)+(n*(30*sizeof(char)))));
-}
+int *qnt = NULL;
+int *num = NULL;
+char *nome = NULL;
 
 int main(){
+    void *pB = NULL;
 
-int *i, *n,*pal;
-char *nome[30];
-
-pal=(malloc(1*sizeof(int));
-
-n=&pal[0];
-*n=0;
+    int *aux = NULL;
 
 
 
-printf("Digite o numeros de pessoas");
+    pB = malloc(sizeof(int)*2);
+        if(pB == NULL){
+            exit(1);
+        }
+    opc = pB;
+    qnt = pB + sizeof(int);
+
+    *qnt = 0;
+
+    do{
+       printf("1 - Novo Contato\n2 - Busca\n3- Apagar Contato\n4 - Listar Contatos\n0 - Sair\n");
+       scanf("%d", aux);
+
+       switch(*aux){
+
+       case 1:
+        novoContato(pB);
+       break;
+
+       case 2:
+        buscaContato(pB);
+       break;
+
+       case 3:
+        apagarContato(pB);
+       break;
+
+       case 4:
+        listarContatos(pB);
+       break;
+
+       case 0:
+       return 0;
+       break;
+
+       default :
+       printf("\nOpcao Invalida\n");
+       }
+
+    }while(opc!=0);
+
+    return 0;
+}
+
+void novoContato(void *pB){
+
+    *qnt ++;
+
+    pB = realloc(buffer, (sizeof(int)*2+(sizeof(char)*20 + sizeof(int))*(*qnt));
+
+    printf("Nome: \n");
+    scanf("%s20", nome);
+
+    printf("Numero: \n");
+    scanf("%d", numero);
+
+    *tamAux = (*tamAux) * (*qnt);
+
+       if(qnt==1){
+    nome = pB + sizeof(int)*2;
+    num = pB + sizeof(int)*2 + sizeof(char)*20;}
+
+           else{
+    nome = pB + sizeof(int)*2+(sizeof(char)*20 + sizeof(int))*(*qnt);
+    num = pB + sizeof(int)*2+(sizeof(char)*20 + sizeof(int))*(*qnt) + sizeof(char)*20;
+
+    }
 
 
 
+}
 
+void buscaContato(void *pB){
+
+}
+
+void apagarContato(void *pB){}
+
+void listarContatos(void *pB){
+
+if(qnt==0){
+printf("\nAgenda Vazia\n");
+break;
+}
+    else{
+
+for(int i=0;i<qnt;i++){
+
+}
+
+
+
+}
+
+void ordenacaoBF(void *pB){}
+
+void ordenacaoInsert(void *pB){}
+}()
