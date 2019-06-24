d28146b28dc3bac9dc2220619c0a1d0bbff9aba7
Dhubleid <42750555+Dhubleid@users.noreply.github.com>
2019-06-24 11:15:12 -0300

    Add files via upload

---
+++ arvoreavl/arvore.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "arvore.h"
+
+void nodeAllocator(BstNode *treePtr) //Alocador dos Nodos
+{
+    if(!(*treePtr = malloc(sizeof(BstNode)))){
+        printf("Error allocating memory, exiting program...\n")
+        exit(1);
+    }
+}
+
+void searchT(agenda *inputSearch, BstNode *treePtr) // função de busca na arvore - retorna ponteiro
+{
+    if(treePtr == NULL){
+        printf("Dados não encontrados\n");
+        return;
+    }
+
+    if(strcmp(agenda->nome, treePtr->data->nome) < 0 ){
+        searchT(agenda, &treePtr->left);
+        return;
+    }
+
+    if (strcmp(agenda->nome, treePtr->data->nome) > 0){
+        searchT(agenda->nome, &treePtr->right);
+    }else
+        *inputSearch = treePtr->data;
+}
+
+void insertData(agenda *inptData, BstNode *treePtr)
+{
+    if(*treePtr == NULL){
+        nodeAllocator(treePtr);
+        treePtr->data = *inptData;
+        treePtr->left = NULL; treePtr->right = NULL;
+        return;
+    }
+
+    if(strcmp(agenda->nome, treePtr->data->nome) < 0 ){
+        insertData(agenda, &treePtr->left);
+        return;
+    }
+
+    if (strcmp(agenda->nome, treePtr->data->nome) > 0){
+        insertData(agenda->nome, &treePtr->right);
+    }else
+        printf("Data already in the tree\n");
+}
+
+void startTree(BstNode *treePtr)
+{
+    *treePtr = NULL;
+}
--- arvoreavl/arvore.h
+++ arvoreavl/arvore.h
@@ -1,6 +1,7 @@
 #ifndef ARVORE_H_INCLUDED
 #define ARVORE_H_INCLUDED
 
+
 struct agenda{
     char nome[30];
     int num;
@@ -17,6 +18,12 @@ struct BstNode {
 typedef struct BstNode BstNode;
 
 
+void searchT(agenda *agenda, BstNode *rootPtr);
 
+void insertData(agenda *inptData, BstNode *treePtr);
+
+void nodeAllocator(BstNode *treePtr);
+
+void startTree(BstNode *treePtr)
 
 #endif // ARVORE_H_INCLUDED
